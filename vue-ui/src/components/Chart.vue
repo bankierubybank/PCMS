0139dab6fdc6362da49c9197fe46032c297a0679
Chatchai Wongdetsakul <58070020@kmitl.ac.th>
2019-01-25 14:13:16 +0700

    Change chart module from vue-google-charts to vue-morris

--- vue-ui/src/components/Chart.vue
+++ vue-ui/src/components/Chart.vue
@@ -2,41 +2,56 @@
   <div class="container">
     <h1>Chart</h1>
     <div class="chart">
-      <GChart type="LineChart" :data="chartData" :options="chartOptions" :resizeDebounce="500"/>
+      <line-chart
+        id="line"
+        :data="chartData"
+        xkey="Timestamp"
+        ykeys='["Value"]'
+        labels='["T1"]'
+        line-color='["#FF6384"]'
+        grid="true"
+        grid-text-weight="bold"
+        resize="true"
+      ></line-chart>
     </div>
   </div>
 </template>
 
 <script>
 import GetServices from "@/services/GetServices";
-import { GChart } from "vue-google-charts";
+import Raphael from "raphael/raphael";
+global.Raphael = Raphael;
+import { LineChart } from "vue-morris";
 export default {
   name: "chart",
   components: {
-    GChart
+    LineChart
   },
   data() {
     return {
-      vms: [],
       chartData: [
-        ["DateTime", "Percentage"],
-        ["2019-01-23T15:00:00+07:00", 0.67],
-        ["2019-01-23T17:00:00+07:00", 0.23],
-        ["2019-01-23T19:00:00+07:00", 0.17],
-        ["2019-01-23T21:00:00+07:00", 0.2],
-        ["2019-01-23T23:00:00+07:00", 0.18],
-        ["2019-01-24T01:00:00+07:00", 0.16],
-        ["2019-01-24T03:00:00+07:00", 0.26],
-        ["2019-01-24T05:00:00+07:00", 0.4],
-        ["2019-01-24T07:00:00+07:00", 0.16],
-        ["2019-01-24T09:00:00+07:00", 0.29]
-      ],
-      chartOptions: {
-        chart: {
-          title: "CPU Usage",
-          subtitle: "CPU usage as a percentage during the interval"
-        }
-      }
+        { Timestamp: "2019-01-23T15:00:00+07:00", Value: 0.67 },
+        { Timestamp: "2019-01-23T17:00:00+07:00", Value: 0.23 },
+        { Timestamp: "2019-01-23T19:00:00+07:00", Value: 0.17 },
+        { Timestamp: "2019-01-23T21:00:00+07:00", Value: 0.2 },
+        { Timestamp: "2019-01-23T23:00:00+07:00", Value: 0.18 },
+        { Timestamp: "2019-01-24T01:00:00+07:00", Value: 0.16 },
+        { Timestamp: "2019-01-24T03:00:00+07:00", Value: 0.26 },
+        { Timestamp: "2019-01-24T05:00:00+07:00", Value: 0.4 },
+        { Timestamp: "2019-01-24T07:00:00+07:00", Value: 0.16 },
+        { Timestamp: "2019-01-24T09:00:00+07:00", Value: 0.29 },
+        { Timestamp: "2019-01-24T11:00:00+07:00", Value: 0.68 },
+        { Timestamp: "2019-01-24T13:00:00+07:00", Value: 0.24 },
+        { Timestamp: "2019-01-24T15:00:00+07:00", Value: 0.24 },
+        { Timestamp: "2019-01-24T17:00:00+07:00", Value: 0.25 },
+        { Timestamp: "2019-01-24T19:00:00+07:00", Value: 0.25 },
+        { Timestamp: "2019-01-24T21:00:00+07:00", Value: 0.24 },
+        { Timestamp: "2019-01-24T23:00:00+07:00", Value: 0.25 },
+        { Timestamp: "2019-01-25T01:00:00+07:00", Value: 0.25 },
+        { Timestamp: "2019-01-25T03:00:00+07:00", Value: 0.24 },
+        { Timestamp: "2019-01-25T05:00:00+07:00", Value: 0.35 },
+        { Timestamp: "2019-01-25T07:00:00+07:00", Value: 0.24 }
+      ]
     };
   },
   mounted() {
@@ -45,7 +60,7 @@ export default {
   methods: {
     async getVMStat(vmName) {
       const response = await GetServices.fetchVMStat(vmName);
-      this.vms = response.data;
+      this.chartData = response.data;
     }
   }
 };
