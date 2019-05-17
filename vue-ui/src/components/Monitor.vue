17578602c0c5d500b1de7b8c200c74315c13ca39
Chatchai Wongdetsakul <58070020@kmitl.ac.th>
2019-05-17 15:21:27 +0700

    Update application's UI/UX

--- vue-ui/src/components/Monitor.vue
+++ vue-ui/src/components/Monitor.vue
@@ -6,59 +6,97 @@
       <b-spinner variant="primary" label="Spinning"></b-spinner>
     </div>
     <div v-else>
-      <b-form @submit="onSubmit">
-        <b-form-select v-model="range" :options="rangeOptions"></b-form-select>
-        <b-button type="submit" variant="primary">Submit</b-button>
-      </b-form>
-      <b-card-group deck>
-        <div v-for="vm in this.queryData" v-bind:key="vm.Name">
-          <b-card :title="vm.Name">
-            <apexchart type="pie" :options="chartOptions" :series="vm.PowerStateSumary"/>
-            <b-button v-b-modal="vm.Name" variant="primary" size="sm">ดูข้อมูลโดยละเอียด</b-button>
+        <b-form-select v-model="range" :options="rangeOptions" @change="onChange()"></b-form-select>
+      <b-row>
+        <b-col>
+          <b-table
+            :items="queryData"
+            :fields="psfields"
+            class="mt-3"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+          >
+            <template slot="FreeUsedRatio" slot-scope="vm">
+              <div>
+                <b-button v-b-modal="vm.item.Name" variant="primary" size="sm">ดูข้อมูลโดยละเอียด</b-button>
 
-            <b-modal :id="vm.Name" :title="vm.Name + ' Stats'" size="lg" hide-footer>
-              <b-container>
-                <b-row>
-                  <b-col>
-                    PowerState
-                    <apexchart
-                      type="line"
-                      :options="lineOptions"
-                      :series="[{ name: vm.Name, data: vm.PowerStateData }]"
-                    />
-                  </b-col>
-                  <b-col>
-                    CPU Usage in Percentage
-                    <apexchart
-                      type="line"
-                      :options="lineOptions"
-                      :series="[{ name: vm.Name, data: vm.CPUData }]"
-                    />
-                  </b-col>
-                </b-row>
-                <b-row>
-                  <b-col>
-                    Memory Usage in Percentage
-                    <apexchart
-                      type="line"
-                      :options="lineOptions"
-                      :series="[{ name: vm.Name, data: vm.MemoryData }]"
-                    />
-                  </b-col>
-                  <b-col>
-                    Disk Usage in KBps
-                    <apexchart
-                      type="line"
-                      :options="lineOptions"
-                      :series="[{ name: vm.Name, data: vm.DiskData }]"
-                    />
-                  </b-col>
-                </b-row>
-              </b-container>
-            </b-modal>
-          </b-card>
-        </div>
-      </b-card-group>
+                <b-modal :id="vm.item.Name" :title="vm.item.Name + ' Stats'" size="lg" hide-footer>
+                  <b-container>
+                    <b-row>
+                      <b-col>
+                        PowerState
+                        <apexchart
+                          type="line"
+                          :options="lineOptions"
+                          :series="[{ name: vm.item.Name, data: vm.item.PowerStateData }]"
+                        />
+                      </b-col>
+                      <b-col>
+                        CPU Usage in Percentage
+                        <apexchart
+                          type="line"
+                          :options="lineOptions"
+                          :series="[{ name: vm.item.Name, data: vm.item.CPUData }]"
+                        />
+                      </b-col>
+                    </b-row>
+                    <b-row>
+                      <b-col>
+                        Memory Usage in Percentage
+                        <apexchart
+                          type="line"
+                          :options="lineOptions"
+                          :series="[{ name: vm.item.Name, data: vm.item.MemoryData }]"
+                        />
+                      </b-col>
+                      <b-col>
+                        Disk Usage in KBps
+                        <apexchart
+                          type="line"
+                          :options="lineOptions"
+                          :series="[{ name: vm.item.Name, data: vm.item.DiskData }]"
+                        />
+                      </b-col>
+                    </b-row>
+                  </b-container>
+                </b-modal>
+              </div>
+            </template>
+          </b-table>
+          <div>
+            Sorting By:
+            <b>{{ sortBy }}</b>, Sort Direction:
+            <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b>
+          </div>
+        </b-col>
+        <b-col>
+          <b-table :items="datastores" :fields="dsfields" class="mt-3">
+        <template slot="FreeUsedRatio" slot-scope="data">
+          <div>
+            <apexchart
+              type="bar"
+              height="100"
+              :options="barchartOptions"
+              :series="[
+                {
+                  name: 'Used Space',
+                  data: [data.item.FreeUsedRatio.UsedSpaceGB]
+                },
+                {
+                  name: 'Free Space',
+                  data: [data.item.FreeUsedRatio.FreeSpaceGB]
+                }
+              ]"
+            />
+          </div>
+        </template>
+      </b-table>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col></b-col>
+        <b-col></b-col>
+      </b-row>
     </div>
   </b-container>
 </template>
@@ -92,15 +130,74 @@ export default {
         { text: "1 Week", value: { weeks: 1 } },
         { text: "1 Month", value: { months: 1 } }
       ],
-      queryData: []
+      queryData: [],
+      psfields: [
+        {
+          key: "Name",
+          label: "Datastore Name"
+        },
+        {
+          key: "PowerStatePercentage",
+          label: "Power On Percentage"
+        },
+        {
+          key: "FreeUsedRatio",
+          label: "Chart"
+        }
+      ],
+      sortBy: "PowerStatePercentage",
+      sortDesc: false,
+      dsfields: [
+        {
+          key: "Name",
+          label: "Datastore Name",
+          sortable: true
+        },
+        {
+          key: "FreeSpaceGB",
+          label: "Free Space in GB",
+          sortable: true
+        },
+        {
+          key: "CapacityGB",
+          label: "Capacity in GB",
+          sortable: true
+        },
+        {
+          key: "UsedSpaceGB",
+          label: "Used Space in GB",
+          sortable: true
+        },
+        {
+          key: "FreeUsedRatio",
+          label: "Chart"
+        }
+      ],
+      datastores: [],
+      barchartOptions: {
+        colors: ["#dc3545", "#28a745"],
+        chart: {
+          stacked: true,
+          stackType: "100%",
+          sparkline: {
+            enabled: true
+          }
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            barHeight: "20%"
+          }
+        }
+      }
     };
   },
   mounted() {
     this.getPowerState();
+    this.getDetailedDatastores();
   },
   methods: {
-    onSubmit(evt) {
-      evt.preventDefault();
+    async onChange() {
       this.loading = true;
       this.queryData = [];
       this.data.forEach(vm => {
@@ -137,10 +234,14 @@ export default {
               return x;
             }
           }).filter(x => x),
-          PowerStateSumary: [
+          PowerStateSummary: [
             PowerOnCount,
             MappingPowerState.length - PowerOnCount
-          ]
+          ],
+          PowerStatePercentage: this.round(
+            (PowerOnCount / MappingPowerState.length) * 100,
+            2
+          )
         });
       });
       this.loading = false;
@@ -165,7 +266,11 @@ export default {
               CPUData: vm.stats.map(x => [new Date(x.timestamp), x.CPU]),
               MemoryData: vm.stats.map(x => [new Date(x.timestamp), x.Memory]),
               DiskData: vm.stats.map(x => [new Date(x.timestamp), x.Disk]),
-              PowerStateSumary: [PowerOnCount, vm.stats.length - PowerOnCount]
+              PowerStateSummary: [PowerOnCount, vm.stats.length - PowerOnCount],
+              PowerStatePercentage: this.round(
+                (PowerOnCount / vm.stats.length) * 100,
+                2
+              )
             });
           });
           this.queryData = this.data;
@@ -180,6 +285,42 @@ export default {
             });
           }
         });
+    },
+    round(value, decimals) {
+      return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+    },
+    async getDetailedDatastores() {
+      this.datastores = [];
+      this.loading = true;
+      let datastores = await GetServices.fetchDatastores().catch(err => {
+        if (err.response.status == 403) {
+          localStorage.removeItem("user");
+          this.$swal("Session Timeout!");
+          this.$router.push({
+            name: "Login"
+          });
+        }
+      });
+      Array.prototype.forEach.call(datastores.data, datastore => {
+        this.datastores.push({
+          Name: datastore.Name,
+          Id: datastore.Id,
+          FreeSpaceGB: this.round(datastore.FreeSpaceGB, 2),
+          CapacityGB: this.round(datastore.CapacityGB, 2),
+          UsedSpaceGB: this.round(
+            datastore.CapacityGB - datastore.FreeSpaceGB,
+            2
+          ),
+          FreeUsedRatio: {
+            FreeSpaceGB: this.round(datastore.FreeSpaceGB, 2),
+            UsedSpaceGB: this.round(
+              datastore.CapacityGB - datastore.FreeSpaceGB,
+              2
+            )
+          }
+        });
+      });
+      this.loading = false;
     }
   }
 };
